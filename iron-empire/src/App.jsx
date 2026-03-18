254cc70594c2b0581427ed513a94b9f3029c8a18
lochani <lochaniketipearachchi@gmail.com>
2026-03-18 19:22:57 +0530

    Update App.js
    Import all the components

--- iron-empire/src/App.jsx
+++ iron-empire/src/App.jsx
@@ -1,2 +1,28 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Trainers from './components/Trainers';
+import Plans from './components/Plans';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Trainers />
+        <Plans />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
